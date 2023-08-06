900591b13ff09e014d5852bdd0145cd9d203d1da
Salim Khan <forsalim@gmail.com>
2023-08-06 13:54:00 +0530

    this sample java gradle project

---
+++ demoGradle2/src/main/java/com/example/demoGradle2/DemoGradle2Application.java
@@ -0,0 +1,13 @@
+package com.example.demoGradle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradle2Application.class, args);
+	}
+
+}
---
+++ demoGradle2/src/test/java/com/example/demoGradle2/DemoGradle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGradle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradle2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
